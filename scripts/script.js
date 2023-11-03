604efb7fa39924e503668c1242ff22b796857247
natcamh <herreranataliacamila@gmail.com>
2023-11-03 08:52:37 -0300

    cambios de nahuel

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var form = document.getElementById("miFormulario");
+
+    if (form) {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+            location.reload();
+        });
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("keydown", function(event) {
+        if (event.key === "F5") {
+            event.preventDefault();
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    });
+});
